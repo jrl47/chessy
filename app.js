0a36207c5332bda4540a416815dfd45116e0ab62
jrl47 <jrl47@users.noreply.github.com>
2022-08-21 11:40:44 -0400

    Updated the chessboard-element import to have a local copy and not depend on some link somewhere.

--- app.js
+++ app.js
@@ -3,42 +3,42 @@
 
 // TODO the index.html file here will need to be adjusted, I think, if I want to run this server
 
-// const http = require('http');
-// const fs = require('fs');
-// const path = require('path');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-// const PORT = process.env.PORT || 13854;
+const PORT = process.env.PORT || 13854;
 
-// http.createServer((request, response) => {
-//     console.log('request ', request.url);
+http.createServer((request, response) => {
+    console.log('request ', request.url);
 
-//     let filePath = '.' + request.url;
-//     if (filePath == './' || filePath == './?authuser=0' /* for google's Web Preview feature */) {
-//         filePath = './index.html';
-//     } else {
-//         filePath = '.' + request.url;
-//     }
+    let filePath = '.' + request.url;
+    if (filePath == './' || filePath == './?authuser=0' /* for google's Web Preview feature */) {
+        filePath = './index.html';
+    } else {
+        filePath = '.' + request.url;
+    }
 
-//     let extname = String(path.extname(filePath)).toLowerCase();
-//     let mimeTypes = {
-//         '.html': 'text/html',
-//         '.js': 'text/javascript',
-//         '.css': 'text/css'
-//     };
-//     let contentType = mimeTypes[extname];
+    let extname = String(path.extname(filePath)).toLowerCase();
+    let mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css'
+    };
+    let contentType = mimeTypes[extname];
 
-//     fs.readFile(filePath, (error, content) => {
-//         if (error) {
-//             response.writeHead(500);
-//             response.end('Error with code: ' + error.code + '.\n');
-//         } else {
-//             response.writeHead(200, { 'Content-Type': contentType });
-//             response.end(content, 'utf-8');
-//         }
-//     });
+    fs.readFile(filePath, (error, content) => {
+        if (error) {
+            response.writeHead(500);
+            response.end('Error with code: ' + error.code + '.\n');
+        } else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
 
-// }).listen(PORT);
-// console.log('App listening on port ' + PORT);
+}).listen(PORT);
+console.log('App listening on port ' + PORT);
 
 
 
